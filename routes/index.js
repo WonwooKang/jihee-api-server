41b05cbeddad459858a903418bec979a29533352
CLOVER-SERVER\Administrator <english793@gmail.com>
2018-08-27 13:38:29 +0900

    forever 스크립트 추가

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/killMySelf', function(req, res, next){
+    throw Error('Killing Test');
+});
+
 module.exports = router;
